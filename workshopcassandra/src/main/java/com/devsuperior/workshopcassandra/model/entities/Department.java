221b101b9080ec57a3ad7573b5422ff18c3f198a
FrankDestro <frank-destro@outlook.com>
2024-05-29 20:30:31 -0300

    feat: configuration initial project cassandra

---
+++ workshopcassandra/src/main/java/com/devsuperior/workshopcassandra/model/entities/Department.java
@@ -0,0 +1,24 @@
+package com.devsuperior.workshopcassandra.model.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(value = "departments")
+public class Department {
+
+    @PrimaryKey
+    private UUID id;
+    private String name;
+}
