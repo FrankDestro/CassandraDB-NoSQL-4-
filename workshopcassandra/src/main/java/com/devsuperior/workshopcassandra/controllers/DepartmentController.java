37b196832af00774d39e1398693ff059a11d372e
FrankDestro <frank-destro@outlook.com>
2024-06-04 18:43:11 -0300

    feat(departamento): adicionar método de inserção no DepartmentService e DepartmentController: feat: Tipo de mudança (feature). departamento: Escopo da mudança (pode ser o nome do módulo ou componente). adicionar método de inserção no DepartmentService e DepartmentController: Resumo da mudança. Corpo da mensagem: Adicionar método insert no DepartmentService para inserir novos departamentos: Descreve a adição do método insert no DepartmentService. Adicionar endpoint POST no DepartmentController para lidar com requisições de inserção de departamentos: Descreve a adição do endpoint POST no DepartmentController.

--- workshopcassandra/src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
+++ workshopcassandra/src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -4,13 +4,12 @@ import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.services.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 import java.util.UUID;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/departments")
@@ -20,14 +19,21 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     @GetMapping
-    public ResponseEntity<List<DepartmentDTO>> findAll () {
+    public ResponseEntity<List<DepartmentDTO>> findAll() {
         List<DepartmentDTO> list = departmentService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<DepartmentDTO> findById (@PathVariable UUID id) {
+    public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id) {
         DepartmentDTO dto = departmentService.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> insert(@RequestBody DepartmentDTO dto) {
+        DepartmentDTO obj = departmentService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
