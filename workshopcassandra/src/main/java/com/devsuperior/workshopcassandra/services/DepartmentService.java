c071d7420bab593fe268dd113591b09fcab83ff2
FrankDestro <frank-destro@outlook.com>
2024-06-04 18:17:50 -0300

    feat(department): add DepartmentController, DepartmentDTO, and DepartmentService 1- Add DepartmentController to handle department-related HTTP requests 2- Add DepartmentDTO to transfer department data 3- Add DepartmentService to manage department business logic 4- Add ResourceNotFoundException to handle cases where a department is not found

--- workshopcassandra/src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ workshopcassandra/src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -3,10 +3,13 @@ package com.devsuperior.workshopcassandra.services;
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Department;
 import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DepartmentService {
@@ -17,5 +20,11 @@ public class DepartmentService {
     public List<DepartmentDTO> findAll() {
         List<Department> list = departmentRepository.findAll();
         return list.stream().map(x -> new DepartmentDTO(x)).toList();
+    }
+
+    public DepartmentDTO findById(UUID id) {
+        Optional<Department> result = departmentRepository.findById(id);
+        Department entity = result.orElseThrow(() -> new ResourceNotFoundException("id not found"));
+        return new DepartmentDTO(entity);
     }
 }
